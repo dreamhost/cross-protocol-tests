c46e15c29ebc955e3d64e99366b8c7377ad11494
kevinchoi-dreamhost <kevin.choi@dreamhost.com>
2013-07-17 16:28:07 -0700

    Finish basic permissions test structure

--- tests/permissions.py
+++ tests/permissions.py
@@ -7,16 +7,30 @@ def create_swift_container_with_acl(acl_headers):
     container = create_valid_name()
     ### Note: Bobtail does not support updating container ACLs using PUT
     swiftconn.put_container(container)
-    swiftconn.post_container(container, acl_headers)
+    swiftconn.post_container(container,acl_headers)
     return container
-
+def create_s3_container_with_acl(acl_headers):
+    # Create S3 container
+    s3conn = get_s3conn()
+    bucket = create_valid_name()
+    s3conn.put_bucket(bucket)
+    return bucket
+def delete_all():
+    # Delete using S3
+    s3conn = get_s3conn()
+    for name in s3_list_buckets(s3conn):
+        bucket = s3conn.get_bucket(name)
+        keys = bucket.list()
+        for key in keys:
+            key.delete()
+        s3conn.delete_bucket(bucket)
+        
 class TestPublicReadSwiftContainer:
     def setUp():
-        ### Create a swift public read container...
+        # Create a swift public read container
         self.bucket = create_swift_container_with_acl({'x-container-read':'.r:*'})
     def tearDown():
-        ### Delete container and objects...
-
+        delete_all()
     def test_default_swift_object():
         # Create Swift object (main user)
         swiftconn = get_swiftconn()
@@ -29,7 +43,7 @@ class TestPublicReadSwiftContainer:
         swiftuser.get_object(self.bucket)
         # Read object using unauthenticated user
         unauthuser = get_unauthuser()
-        unauthuser.get_object(self.bucket)
+        unauthuser.get_contents(self.bucket)
     def test_default_s3_object():
         # Create S3 object (main user)
         s3conn = get_s3conn()
@@ -42,13 +56,12 @@ class TestPublicReadSwiftContainer:
         swiftuser.get_object(self.bucket)
         # Read object using unauthenticated user
         unauthuser = get_unauthuser()
-        unauthuser.get_object(self.bucket)
-
+        unauthuser.get_contents(self.bucket)
     def test_public_read_s3_object():
         # Create public read S3 object (main user)
         s3conn = get_s3conn()
-        s3conn.put_object(self.bucket,'test.txt','test text here')
-        ###s3conn.set_object_acl('ACL HERE')
+        s3conn.put_object(self.bucket,'test','test object')
+        s3conn.add_public_acl('WRITE',self.bucket,'test')
         # Read object using S3 (second user)
         s3user = get_s3user()
         s3user.get_object(self.bucket)
@@ -57,12 +70,12 @@ class TestPublicReadSwiftContainer:
         swiftuser.get_object(self.bucket)
         # Read object using unauthenticated user
         unauthuser = get_unauthuser()
-        unauthuser.get_object(self.bucket)
+        unauthuser.get_contents(self.bucket)
     def test_private_read_s3_object():
         # Create public read S3 object (main user)
         s3conn = get_s3conn()
-        s3conn.put_object(self.bucket,'test.txt','test text here')
-        ###s3conn.set_object_acl('ACL HERE')
+        s3conn.put_object(self.bucket,'test','test object')
+        s3conn.add_private_acl('READ',USERNAME,self.bucket,'test')
         # Read object using S3 (second user)
         s3user = get_s3user()
         s3user.get_object(self.bucket)
@@ -71,12 +84,12 @@ class TestPublicReadSwiftContainer:
         swiftuser.get_object(self.bucket)
         # Read object using unauthenticated user
         unauthuser = get_unauthuser()
-        unauthuser.get_object(self.bucket)
+        unauthuser.get_contents(self.bucket)
     def test_public_full_control_s3_object():
         # Create public read S3 object (main user)
         s3conn = get_s3conn()
-        s3conn.put_object(self.bucket,'test.txt','test text here')
-        ###s3conn.set_object_acl('ACL HERE')
+        s3conn.put_object(self.bucket,'test','test object')
+        s3conn.add_public_acl('FULL_CONTROL',self.bucket,'test')
         # Read object using S3 (second user)
         s3user = get_s3user()
         s3user.get_object(self.bucket)
@@ -85,12 +98,12 @@ class TestPublicReadSwiftContainer:
         swiftuser.get_object(self.bucket)
         # Read object using unauthenticated user
         unauthuser = get_unauthuser()
-        unauthuser.get_object(self.bucket)
+        unauthuser.get_contents(self.bucket)
     def test_private_full_control_s3_object():
         # Create public read S3 object (main user)
         s3conn = get_s3conn()
-        s3conn.put_object(self.bucket,'test.txt','test text here')
-        ###s3conn.set_object_acl('ACL HERE')
+        s3conn.put_object(self.bucket,'test','test object')
+        s3conn.add_private_acl('FULL_CONTROL',USERNAME,self.bucket,'test')
         # Read object using S3 (second user)
         s3user = get_s3user()
         s3user.get_object(self.bucket)
@@ -99,16 +112,15 @@ class TestPublicReadSwiftContainer:
         swiftuser.get_object(self.bucket)
         # Read object using unauthenticated user
         unauthuser = get_unauthuser()
-        unauthuser.get_object(self.bucket)
+        unauthuser.get_contents(self.bucket)
     ### TEST ACLs??
 
 class TestPublicWriteSwiftContainer:
     def setUp():
-        ### Create a swift public read container...
+        # Create a swift public read container
         self.bucket = create_swift_container_with_acl({'x-container-write':'.r:*'})
     def tearDown():
-        ### Delete container and objects...
-
+        delete_all()
     def test_default_swift_object():
         # Create Swift object (main user)
         swiftconn = get_swiftconn()
@@ -129,24 +141,135 @@ class TestPublicWriteSwiftContainer:
         swiftconn.get_container(self.bucket)
         ### eq test
 
+        # Create Swift object (main user)
+        swiftconn.put_object(self.bucket,'test','test object')
+        # Delete object with unauthenticated user
+        unauthuser = get_unauthuser()
+        unauthuser.delete_object(self.bucket,'test')
+        # Check that bucket is empty
+        swiftconn.get_container(self.bucket)
+        ### eq test
+
         # Create Swift object with second user
         swiftuser.put_object(self.bucket,'test','test object')
         # Create S3 object with second user
         s3user.put_object(self.bucket,'test','test object')
-        
+        # Create S3 object with unauthenticated user
+        unauthuser.put_object(self.bucket,'test','test object')
+
         ### Test again with unauthenticated user
     def test_default_s3_object():
+        # Create S3 object (main user)
+        s3conn = get_swiftconn()
+        s3conn.put_object(self.bucket,'test','test object')
+        # Delete object with Swift second user
+        swiftuser = get_swiftuser()
+        swiftuser.delete_object(self.bucket, 'test')
+        # Check that container is empty
+        swiftconn.get_container(self.bucket)
+        ### eq test
+
+        # Create S3 object (main user)
+        s3conn = get_swiftconn()
+        s3conn.put_object(self.bucket,'test','test object')
+        # Delete object with S3 second user
+        s3user = get_swiftuser()
+        s3user.delete_object(self.bucket, 'test')
+        # Check that container is empty
+        swiftconn.get_container(self.bucket)
+        ### eq test
+
+        # Create S3 object (main user)
+        s3conn = get_swiftconn()
+        s3conn.put_object(self.bucket,'test','test object')
+        # Delete object with unauthenticated user
+        unauthuser = get_unauthuser()
+        unauthuser.delete_object(self.bucket,'test')
+        # Check that bucket is empty
+        swiftconn.get_container(self.bucket)
+        ### eq test
+
+        # Create Swift object with second user
+        swiftuser.put_object(self.bucket,'test','test object')
+        # Create S3 object with second user
+        s3user.put_object(self.bucket,'test','test object')
+        # Create S3 object with unauthenticated user
+        unauthuser.put_object(self.bucket,'test','test object')
+
+
+        ### Test again with unauthenticated user
+class TestPrivateReadSwiftContainer:
+    def setUp():
+        ### Create a Swift public read container...
+        self.bucket = create_swift_container_with_acl({'x-container-read':'SECOND USER, MAIN ACCOUNT'})
+    def tearDown():
+        ### Delete container and objects...
+class TestPrivateWriteSwiftContainer:
+    def setUp():
+        ### Create a Swift public read container...
+        self.bucket = create_swift_container_with_acl({'x-container-write':'SECOND USER, MAIN ACCOUNT'})
+    def tearDown():
+        ### Delete container and objects...
+
+
+class TestPublicWriteS3Bucket:
+    def setUp():
+        # Create an S3 public write bucket
+        self.bucket = create_s3_container_with_acl()
+    def tearDown():
+        ### Delete container and objects...
+
+    ### SET S3 PERMISSIONS? DO LATER
+    def test_default_swift_object():
+        
         # Create Swift object (main user)
         swiftconn = get_swiftconn()
         swiftconn.put_object(self.bucket,'test','test object')
         # Delete object with Swift second user
+        swiftuser = get_swiftuser()
+        swiftuser.delete_object(self.bucket, 'test')
         # Check that container is empty
+        swiftconn.get_container(self.bucket)
+        ### eq test
 
         # Create Swift object (main user)
+        swiftconn.put_object(self.bucket,'test','test object')
         # Delete object with S3 second user
+        s3user = get_s3user()
+        s3user.delete_object(self.bucket, 'test')
         # Check that bucket is empty
+        swiftconn.get_container(self.bucket)
+        ### eq test
+
+        # Create Swift object (main user)
+        swiftconn.put_object(self.bucket,'test','test object')
+        # Delete object with unauthenticated user
+        unauthuser = get_unauthuser()
+        unauthuser.delete_object(self.bucket,'test')
+        # Check that bucket is empty
+        swiftconn.get_container(self.bucket)
+        ### eq test
 
         # Create Swift object with second user
+        swiftuser.put_object(self.bucket,'test','test object')
         # Create S3 object with second user
+        s3user.put_object(self.bucket,'test','test object')
+        # Create S3 object with unauthenticated user
+        unauthuser.put_object(self.bucket,'test','test object')
 
         ### Test again with unauthenticated user
+class TestPublicReadS3Bucket:
+
+class TestPrivateWriteS3Bucket:
+    def setUp():
+        self.bucket=create_s3_container_with_acl({'ACL HERE'})
+    def tearDown():
+        ### Delete bucket and objects
+    ### Similar to private write swift
+class TestPrivateReadS3Bucket:
+    # Just try to list the buckets
+
+class TestPublicFullControlS3Bucket:
+    # Same as public write control...
+
+class TestPrivateFullControlS3Bucket:
--- tests/wrapper.py
+++ tests/wrapper.py
@@ -87,81 +87,141 @@ def get_swiftuser():
 
 class S3Conn(S3Connection):
     def head_account(self):
+        # Get account stats (returns headers in dictionary format)
         resp = self.make_request('HEAD')
         if resp.status < 200 or resp.status >= 300:
             raise S3ResponseError(resp.status, resp.reason, resp.read)
-        return resp
+        headers = {}
+        return headers[header.lower()] = value for header, value in resp.getheaders()
     def head_bucket(self, bucket):
+        # Get bucket stats (returns headers in dictionary format)
         resp = self.make_request('HEAD', bucket)
         if resp.status < 200 or resp.status >= 300:
             raise S3ResponseError(resp.status, resp.reason, resp.read)
-        return resp
+        headers = {}
+        return headers[header.lower()] = value for header, value in resp.getheaders()
     def head_object(self, bucket, name):
+        # Get object stats (returns headers in dictionary format)
         resp = self.make_request('HEAD')
         if resp.status < 200 or resp.status >= 300:
             raise S3ResponseError(resp.status, resp.reason, resp.read)
-        return resp
-    def get_account(self):
-        resp = self.make_request('GET')
-        if resp.status < 200 or resp.status >= 300:
-            raise S3ResponseError(resp.status, resp.reason, resp.read)
-        return resp
-    def get_bucket(self, bucket):
-        resp = self.make_request('GET', bucket)
-        if resp.status < 200 or resp.status >= 300:
-            raise S3ResponseError(resp.status, resp.reason, resp.read)
-        return resp
-    def get_object(self, bucket, filename):
+        headers = {}
+        return headers[header.lower()] = value for header, value in resp.getheaders()
+    def list_buckets(self):
+        # Returns list of buckets
+        buckets = self.get_all_buckets()
+        list_of_buckets = [bucket.name for bucket in buckets]
+        return list_of_buckets
+    def list_objects(self, bucket):
+        # Returns list of objects in a bucket
+        bucket = s3conn.get_bucket(name)
+        list_of_objects = [obj.key for obj in bucket.list()]
+        return list_of_objects
+    def get_contents(self, bucket, filename):
+        # Return object contents
         resp = self.make_request('GET', bucket, filename)
         if resp.status < 200 or resp.status >= 300:
             raise S3ResponseError(resp.status, resp.reason, resp.read)
-        return resp
+        return resp.read()
     def delete_bucket(self, bucket):
         resp = self.make_request('DELETE', bucket)
         if resp.status < 200 or resp.status >= 300:
             raise S3ResponseError(resp.status, resp.reason, resp.read)
-        return resp
     def delete_object(self, bucket, filename):
         resp = self.make_request('DELETE', bucket, filename)
         if resp.status < 200 or resp.status >= 300:
             raise S3ResponseError(resp.status, resp.reason, resp.read)
-        return resp
+    def put_bucket(self, bucket):
+        resp = self.make_request('PUT', bucket)
+        if resp.status < 200 or resp.status >= 300:
+            raise S3ResponseError(resp.status, resp.reason, resp.read)
+        ## EASIER?
+        s3conn.create_bucket(bucket)
+    def put_object(self, bucket, filename, data):
+        resp = self.make_request('PUT', bucket, filename, data)
+        if resp.status < 200 or resp.status >= 300:
+            raise S3ResponseError(resp.status, resp.reason, resp.read)
+        ## EASIER?
+        b = s3conn.get_bucket(bucket)
+        k = Key(b)
+        k.key = filename
+        k.set_contents_from_string(data)
     def post_account(self):
+        # Unnecessary?
         resp = self.make_request('POST')
         if resp.status < 200 or resp.status >= 300:
             raise S3ResponseError(resp.status, resp.reason, resp.read)
         return resp
     def post_bucket(self, bucket):
+        # Unnecessary?
         resp = self.make_request('POST', bucket)
         if resp.status < 200 or resp.status >= 300:
             raise S3ResponseError(resp.status, resp.reason, resp.read)
         return resp
     def post_object(self, bucket, filename):
+        # Unnecessary?
         resp = self.make_request('POST', bucket, filename)
         if resp.status < 200 or resp.status >= 300:
             raise S3ResponseError(resp.status, resp.reason, resp.read)
         return resp
-    def put_bucket(self, bucket):
-        resp = self.make_request('PUT', bucket)
-        if resp.status < 200 or resp.status >= 300:
-            raise S3ResponseError(resp.status, resp.reason, resp.read)
+    def get_acl(self, bucket, filename=None):
+        b = self.get_bucket(bucket)
+        if filename:
+            k = Key(b)
+            k.key = filename
+            return k.get_acl()
+        return b.get_acl()
+    def add_public_acl(self, permission, bucket, filename=None):
+        grant = Grant(permission=permission, type='Group', uri='http://acs.amazonaws.com/groups/global/AllUsers')
+        b = self.get_bucket(bucket)
+        if filename:
+            k = Key(b)
+            k.key = filename
+            policy = k.get_acl()
+            policy.acl.add_grant(grant)
+            k.set_acl(policy)
+            return k.get_acl()
+        policy = b.get_acl()
+        policy.acl.add_grant(grant)
+        b.set_acl(policy)
+        return b.get_acl()
+    def add_private_acl(self, permission, username, bucket, filename=None):
+        grant = Grant(permission=permission, type='CanonicalUser', id=username, display_name=username)
+        b = self.get_bucket(bucket)
+        if filename:
+            k = Key(b)
+            k.key = filename
+            policy = k.get_acl()
+            policy.acl.add_grant(grant)
+            k.set_acl(policy)
+            return k.get_acl()
+        policy = b.get_acl()
+        policy.acl.add_grant(grant)
+        b.set_acl(policy)
+        return b.get_acl()
+class SwiftConn(Connection):
+    def list_containers(self):
+        # Returns list of containers
+        containers = self.get_account()[1]
+        list_of_containers = [container_dict[u'name'] for container_dict in containers]
+        return list_of_containers
+    def list_objects(self, container):
+        # Returns list of objects
+        objects = self.get_container(name)[1]
+        list_of_objects = [object_dictionary[u'name'] for object_dictionary in objects]
+        return list_of_objects
+    def get_contents(self, container, filename):
+        # Return object get_contents
+        return self.get_object(container, filename)[1]
+class HTTPConn(httplib.HTTPConnection):
+    def get_contents(self, container, filename):
+        self.request('GET', '/'+container+'/'+filename)
+        resp = self.getresponse()
         return resp
-    def put_object(self, bucket, filename, data):
-        resp = self.make_request('PUT', bucket, filename, data)
-        if resp.status < 200 or resp.status >= 300:
-            raise S3ResponseError(resp.status, resp.reason, resp.read)
+    def put_object(self, container, filename, data):
+        self.request('GET','/'+container+'/'+filename, body=data)
+        resp = self.getresponse()
         return resp
-    def get_acl(self, bucket, filename=None, acl):
-        resp = self.make_request('GET', bucket, filename, data=acl, query_arges='acl')
-        if resp.status < 200 or resp.status >= 300:
-            raise S3ResponseError(resp.status, resp.reason, resp.read)
-        return resp
-    def set_acl(self, bucket, filename=None, acl):
-        resp = self.make_request('POST', bucket, filename, data=acl, query_arges='acl')
-        if resp.status < 200 or resp.status >= 300:
-            raise S3ResponseError(resp.status, resp.reason, resp.read)
-        return resp
-
 # For more parameters:
 # https://github.com/boto/boto/blob/develop/boto/s3/connection.py
 def s3conn():
