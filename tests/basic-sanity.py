adf91b5e05fe8551c2abc803ce74c73a3b92db8c
kevinchoi-dreamhost <kevin.choi@dreamhost.com>
2013-07-22 10:48:17 -0700

    add cross-bucket write permissions test to perms

--- tests/basic-sanity.py
+++ tests/basic-sanity.py
@@ -12,6 +12,21 @@ import yaml
 import sys
 import os
 
+import tools
+from tools import get_config
+from tools import assert_raises
+from tools import create_valid_name
+from tools import get_s3conn
+from tools import get_swiftconn
+from tools import get_s3user
+from tools import get_swiftuser
+from tools import get_unauthuser
+
+import httplib
+
+import unittest
+from nose.tools import eq_ as eq
+
 ## FROM https://github.com/ceph/swift/blob/master/test/__init__.py
 def get_config():
     """
